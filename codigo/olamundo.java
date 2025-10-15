21e31a7d19b81a290642a983fad7721c9525f398
joaoyoyo-dcomp <joao.yoyo@dcomp.ufs.br>
2025-10-15 14:35:25 -0300

    Create olamundo.java

---
+++ codigo/olamundo.java
@@ -0,0 +1,10 @@
+/*fazendo primeiro código Hello World no java
+public declarado no início do código precisa ser igual ao número do arquivo
+toda variável em java possui uma classe
+
+*/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+}
